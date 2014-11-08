3322f5cabec790784adce6ef620fe56f50e739c1
Seravy <seravy@gmail.com>
2014-11-08 18:15:29 +0800

    Added load project logic and nuget package.

--- Doc.Net.Framework/Compiler.cs
+++ Doc.Net.Framework/Compiler.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Build.Evaluation;
 
 namespace Doc.Net.Framework
 {
     public class Compiler
     {
+        public Compiler()
+        {
+            
+        }
+
+        public void LoadProject(string projectLocation)
+        {
+            var project = new Project();
+            project.Load(projectLocation);
+            var items = project.Items.Where(o => o.ItemType == "Content");
+        }
+
     }
 }
--- Dot.Net.Tests/Framework.cs
+++ Dot.Net.Tests/Framework.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using Doc.Net.Framework;
 using NUnit.Framework;
 
 namespace Dot.Net.Tests
@@ -9,6 +11,16 @@ namespace Dot.Net.Tests
         public void Temp()
         {
             Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void CompileThisProject()
+        {
+            var comp = new Compiler();
+            var name = Assembly.GetCallingAssembly();
+
+            comp.LoadProject(@"C:\Dev\git\Doc.Net\Doc.Net.Framework\Doc.Net.Framework.csproj");
+
         }
     }
 }
