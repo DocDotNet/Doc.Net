f7a1f981d9b04e7d3acfee406e406fcf4c9700b7
Seravy <seravy@gmail.com>
2014-12-13 12:58:27 +0800

    Add missing content pages.

---
+++ Doc.Net.Framework/Content/HtmlContent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doc.Net.Framework.Content
+{
+    public class HtmlContent : IContent
+    {
+        private string html;
+
+        public HtmlContent(string html)
+        {
+            this.html = html;
+        }
+
+        public string ToHtml()
+        {
+            return this.html;
+        }
+    }
+}
---
+++ Doc.Net.Framework/Content/Page.cs
@@ -0,0 +1,14 @@
+﻿namespace Doc.Net.Framework.Content
+{
+    public class Page
+    {
+        public string Id { get; set; }
+
+        public IContent Content { get; set; }
+
+        public string ContentInHtml()
+        {
+            return Content.ToHtml();
+        }
+    }
+}
